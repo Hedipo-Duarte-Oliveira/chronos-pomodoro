df58e15b876f5b3553cfcf3c53750abd5cd3b1bf
Hedipo Duarte <hedipomissoes@gmail.com>
2026-01-27 18:23:39 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
-function App() {
-  return <h1>Ola Mundo(do app) </h1>;
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+  console.log('oi');
+  return (
+    <div>
+      <Heading />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis vero
+        explicabo nemo et aliquid officiis adipisci reiciendis enim id,
+        consectetur odio reprehenderit, molestias dignissimos soluta, nisi
+        possimus earum consequatur sint.
+      </p>
+    </div>
+  );
 }
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import QualquerCoisa from './App';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QualquerCoisa />
-
-    <h1>Ola Mundo! </h1>
+    <App />
   </StrictMode>,
 );
